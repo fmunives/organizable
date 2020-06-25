773bb61d5dee8f6dba5631259185d4f91eb7e93c
Freddy Munive <munivef@yahoo.com>
2020-06-24 20:34:58 -0500

    adding boards and closed boards

---
+++ js/menuBoards.js
@@ -0,0 +1,49 @@
+myBoards = document.getElementById("myBoards");
+closedBoards = document.getElementById("closedBoards");
+myProfile = document.getElementById("myProfile");
+logout = document.getElementById("logout");
+
+allButtons = document.querySelectorAll(".option");
+allSections = document.querySelectorAll(".sections");
+
+allButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    removeActiveOthersButtons();
+    event.target.classList.add("active");
+    const idButton = event.target.id;
+    switchButton(idButton);
+  });
+});
+
+function showCurrentlySection(section) {
+  section.classList.add("section-active");
+}
+
+function hideOthersSections() {
+  allSections.forEach((section) => section.classList.remove("section-active"));
+}
+
+function removeActiveOthersButtons() {
+  allButtons.forEach((button) => button.classList.remove("active"));
+}
+
+function switchButton(button) {
+  switch (button) {
+    case "btnMyBoards":
+      hideOthersSections();
+      showCurrentlySection(myBoards);
+      break;
+    case "btnClosedBoard":
+      hideOthersSections();
+      showCurrentlySection(closedBoards);
+      break;
+    case "btnProfile":
+      hideOthersSections();
+      showCurrentlySection(myProfile);
+      break;
+    default:
+      hideOthersSections();
+      showCurrentlySection(logout);
+      break;
+  }
+}
