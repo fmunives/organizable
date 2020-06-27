9038d086f5fe87ff42d89639c7c2723d1ecac6e5
humberto delgado <dehumberto@gmail.com>
2020-06-26 22:19:06 -0500

    Edit and delete user profile
    fix signup to save first and last name

--- js/menuBoards.js
+++ js/menuBoards.js
@@ -51,10 +51,10 @@ function switchButton(button) {
   }
 }
 
-async function getCurrentUser() {
-  const currentToken = localStorage.getItem("token");
-  const currentUserId = localStorage.getItem("id");
+const currentToken = localStorage.getItem("token");
+const currentUserId = localStorage.getItem("id");
 
+async function getCurrentUser() {
   const options = {
     method: "GET",
     headers: {
@@ -82,6 +82,73 @@ async function getCurrentUser() {
   textEmail.value = email;
   textFirstName.value = firstName;
   textLastName.value = lastName;
+}
+
+const signupForm = document.querySelector(".sign-up__form");
+
+signupForm.addEventListener("submit", () => {
+  event.preventDefault();
+  const userNewInfo = {
+    user: {
+      username: document.getElementById("username").value,
+      email: document.getElementById("email").value,
+      first_name: document.getElementById("first-name").value,
+      last_name: document.getElementById("last-name").value,
+    },
+  };
+  update(`http://localhost:3000/users/${currentUserId}`, userNewInfo);
+});
+
+async function update(url, user) {
+  const options = {
+    method: "PATCH",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token token="${currentToken}"`,
+    },
+  };
+
+  const response = await fetch(url, options).then((res) => res.json());
+  console.log(response);
+
+  if (!response.id) {
+    console.log(response);
+  } else {
+    console.log("data cambiada");
+    setTimeout(function () {
+      alert("User updated");
+      switchButton("btnProfile");
+    }, 1000);
+    //localStorage.setItem("id", response.id);
+    //localStorage.setItem("token", response.token);
+    //window.location.replace("boards.html");
+  }
+}
+
+buttonDeleteUser = document.getElementById("delete-user-button");
+buttonDeleteUser.onclick = () =>
+  destroyUser(`http://localhost:3000/users/${currentUserId}`);
+
+async function destroyUser(url) {
+  console.log(url);
+  const options = {
+    method: "DELETE",
+    headers: {
+      Authorization: `Token token="${currentToken}"`,
+    },
+  };
+  const response = await fetch(url, options);
+
+  if (response.ok) {
+    console.log("user deleted");
+    setTimeout(function () {
+      alert("User deleted");
+      switchButton("btnLogOut");
+    }, 1000);
+  } else {
+    console.log(response);
+  }
 }
 
 function logOut() {
